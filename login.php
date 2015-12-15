7787b66fcc92edcb2b287b0e236d673bac922379
mitapein <mita250193@live.com>
2015-12-15 17:19:12 +0100

    added login page

--- login.php
+++ login.php
@@ -1,6 +1,34 @@
 <html>
 <head>
+<title>Login</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<script type="text/javascript" src="jquery-1.11.3.min.js" ></script>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 </head>
 <body>
+
+<div  class="container" style="padding-top:20px;">
+      <form action="loginvalidate.php" method="POST" class="form-signin" style="max-width: 300px;padding: 15px;margin: 0 auto;background-color:#C7C7C7;">
+        <h2 class="form-signin-heading" style="margin-bottom: 10px;">Please sign in</h2>
+        <label for="inputEmail" class="sr-only">Email address</label>
+        <input name="username" id="inputEmail" class="form-control" placeholder="Email address" required="" autofocus="" type="email">
+        <label for="inputPassword" class="sr-only">Password</label>
+        <input name="password" id="inputPassword" class="form-control" placeholder="Password" required="" type="password">
+        <button name="log-in" class="btn btn-lg btn-primary btn-block" type="submit" style="margin-top:15px;">Sign in</button>
+      </form>
+	  <div id="loginInfo" class="container bg-danger" style="max-width: 300px;padding: 15px;margin: 0 auto;display:<?php if(isset ($_GET["error"])){
+		  if($_GET["error"]==1){
+			  echo "";
+		  } else {
+			  echo "none";
+		  }
+	  } else {
+		  echo "none";
+	  }
+	  
+	  ?>;">Invalid login attempt!
+	  </div>
+    </div>
 </body>
 </html>
---
+++ loginvalidate.php
@@ -0,0 +1,23 @@
+<?php
+if(isset ($_POST["log-in"])){	
+	include "connection.php";
+	$name=$_POST["username"];
+	$password=md5($_POST["password"]);
+	$query="SELECT * FROM users WHERE username='".$name."' AND passhash='".$password."'";
+	$upit=$db->query($query);
+	if($upit->rowCount()==0){
+		$query="INSERT INTO users (username, passhash) VALUES ('".$name."','".$password."')";
+		if(!$res=$db->exec($query)){
+			echo "Registered";
+			header("location:http://localhost/itehZad1/edit.php");
+		} else {
+			 echo "sql Error";
+			header("location:http://localhost/itehZad1/login.php?error=1");
+		}
+	} else {
+		header("location:http://localhost/itehZad1/edit.php");	
+		$db=null;		
+		die();
+	}
+}
+?>
